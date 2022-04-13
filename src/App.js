851367cbdaf19749bf01c09462013effddd16474
rolanusonov <rolanusonov031209@gmail.com>
2022-04-13 10:20:39 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>HELLO , SHIRIN</h1>
+
     </div>
   );
 }
--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Script = () => {
     return (
         <div>
-            
+            <h1>rolan</h1>
         </div>
     );
 };
