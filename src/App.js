35d285399a048bc8f03c12115f90ba0a06a7f3dd
shirin15 <imengazyevashika@gmail.com>
2022-04-13 14:26:37 +0300

    dsfd

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>HELLO , SHIRIN</h1>
-<<<<<<< HEAD
-         <p>hello world</p>
+     <h1>hello</h1>
      </div>
-=======
-        <p>hello world</p>
-        <p>rolan</p>
-    </div>
->>>>>>> d671ea8b45d3335fd85ac82dc6aa407d9dcd49df
   );
 }
 
